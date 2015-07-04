fdfc1e6622551bebcc28b5df73f63f1d301ec4fe
Staehler, Michael <michael.staehler@opitz-consulting.com>
2015-07-04 11:06:24 +0200

    cleanup

--- src/main/java/de/fred4jupiter/spring/boot/data/jpa/Customer.java
+++ src/main/java/de/fred4jupiter/spring/boot/data/jpa/Customer.java
@@ -18,10 +18,6 @@ public class Customer {
 		return id;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public String getName() {
 		return name;
 	}
--- src/main/java/de/fred4jupiter/spring/boot/data/jpa/CustomerRepository.java
+++ src/main/java/de/fred4jupiter/spring/boot/data/jpa/CustomerRepository.java
@@ -6,6 +6,4 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 	Customer findByName(String name);
 
-	
-
 }
